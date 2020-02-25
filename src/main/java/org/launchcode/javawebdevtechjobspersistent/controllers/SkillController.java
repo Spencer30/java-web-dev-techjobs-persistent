9058f3afdfe4d82e0d05a99995de05bce540b089
Spencer Lyons <sjl8r8@gmail.com>
2020-02-25 14:38:58 -0600

    finished part2 - Persisting Employers and Skills

---
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
@@ -0,0 +1,39 @@
+package org.launchcode.javawebdevtechjobspersistent.controllers;
+
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("skills")
+public class SkillController {
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @GetMapping("add")
+    public String displayAddSkillForm(Model model) {
+        model.addAttribute(new Skill());
+        return "skills/add";
+    }
+
+    @PostMapping("add")
+    public String processAddSkillForm(@ModelAttribute @Valid Skill newSkill,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "skills/add";
+        }
+        skillRepository.save(newSkill);
+        return "redirect:../add";
+    }
+}
