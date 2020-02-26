e7adf5bd16b09008a9ab452de7e5c7408923de56
Spencer Lyons <sjl8r8@gmail.com>
2020-02-26 10:13:31 -0600

    finished most of  part3 - Setting Up a One-to-Many Relationship

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
 import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
 import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,12 +13,16 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private JobRepository jobRepository;
 
     @Autowired
     private EmployerRepository employerRepository;
@@ -49,13 +55,21 @@ public class HomeController {
             model.addAttribute("title", "Add Job");
             return "add";
         }
+        Optional<Employer> result = employerRepository.findById(employerId);
+        Employer employer = result.get();
+        newJob.setEmployer(employer);
+        System.out.println(newJob.getEmployer());
 
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
-
+        Optional<Job> findJob = jobRepository.findById(jobId);
+        Job selectedJob = findJob.get();
+        System.out.println(selectedJob.getSkills());
+        model.addAttribute("job", selectedJob);
         return "view";
     }
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,17 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
     @NotBlank(message = "Location is required")
     @Size(min = 2, max = 50, message = "Location must be between 2 and 50 characters")
     private String location;
 
     public Employer(String location) {
+        super();
         this.location = location;
     }
 
@@ -23,5 +32,9 @@ public class Employer extends AbstractEntity {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 }
